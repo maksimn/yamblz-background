5e209a42e45ab70f806b6e272820589ce533b4b2
maksimn <maksim1989sovunion@yandex.ru>
2018-07-22 16:44:22 +0300

    HorizontalCollageStrategy

--- app/src/main/java/ru/yandex/yamblz/loader/HorizontalCollageStrategy.java
+++ app/src/main/java/ru/yandex/yamblz/loader/HorizontalCollageStrategy.java
@@ -7,6 +7,34 @@ import java.util.List;
 public class HorizontalCollageStrategy implements CollageStrategy {
     @Override
     public Bitmap create(List<Bitmap> bitmaps) {
-        return null;
+        int resultWidth = 0;
+        int resultHeight = 0;
+
+        for (Bitmap b: bitmaps) {
+            resultWidth += b.getWidth();
+            resultHeight = b.getHeight() > resultHeight ? b.getHeight() : resultHeight;
+        }
+
+        Bitmap resultBitmap = Bitmap.createBitmap(resultWidth, resultHeight,
+                Bitmap.Config.ARGB_8888);
+
+        for (int i = 0, start = 0; i < bitmaps.size(); i++) {
+            Bitmap bmp = bitmaps.get(i);
+
+            int w = bmp.getWidth();
+            int h = bmp.getHeight();
+
+            for (int j = 0; j < w; j++) {
+                for (int k = 0; k < h; k++) {
+                    int pixel = bmp.getPixel(j, k);
+
+                    resultBitmap.setPixel(start + j, k, pixel);
+                }
+            }
+
+            start += w;
+        }
+
+        return resultBitmap;
     }
 }
