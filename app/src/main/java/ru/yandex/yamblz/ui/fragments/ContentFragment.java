2216379d44c556884880a751016652032c2f0aa1
maksimn <maksim1989sovunion@yandex.ru>
2018-07-27 14:09:20 +0300

    Fix wrong recyclerview support version

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -7,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -38,7 +37,6 @@ public class ContentFragment extends BaseFragment {
     @Override
     public void onStart() {
         super.onStart();
-        ImageView collageImageView = (ImageView)getView().findViewById(R.id.collageImageView);
 
         List<CollageInfo> collageInfos = populateCollageInfo();
 
