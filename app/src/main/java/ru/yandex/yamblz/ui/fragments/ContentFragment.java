895abb40666c3b52b0f6544d1aba93419c33f36d
maksimn <maksim1989sovunion@yandex.ru>
2018-07-27 06:34:11 +0300

    Collages JSON in raw folder

--- app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
+++ app/src/main/java/ru/yandex/yamblz/ui/fragments/ContentFragment.java
@@ -9,11 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 
 import ru.yandex.yamblz.R;
-import ru.yandex.yamblz.loader.CollageLoader;
-import ru.yandex.yamblz.loader.CollageLoaderManager;
 import ru.yandex.yamblz.ui.viewModels.CollageViewModel;
 
 public class ContentFragment extends BaseFragment {
@@ -27,11 +30,38 @@ public class ContentFragment extends BaseFragment {
     @Override
     public void onStart() {
         super.onStart();
-
         ImageView collageImageView = (ImageView)getView().findViewById(R.id.collageImageView);
         CollageViewModel model = ViewModelProviders.of(this).get(CollageViewModel.class);
+
+        String collagesJson = readCollagesJsonFromRaw();
+
         model.getData().observe(this, bitmap -> {
             collageImageView.setImageBitmap(bitmap);
         });
+    }
+
+    private String readCollagesJsonFromRaw() {
+        InputStream is = getResources().openRawResource(R.raw.collages_data);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[10000];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        } catch (Exception e) {
+            return null;
+        } finally {
+            try {
+                is.close();
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        String jsonString = writer.toString();
+
+        return jsonString;
     }
 }
